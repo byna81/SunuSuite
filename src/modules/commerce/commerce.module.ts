c2216f978e92dfac94571994383c871f8bbf3130
byna81 <fofana.naby81@gmail.com>
2026-03-18 16:08:40 +0100

    Update commerce.module.ts

--- src/modules/commerce/commerce.module.ts
+++ src/modules/commerce/commerce.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 
-// Categories
 import { CategoriesController } from './categories/categories.controller';
 import { CategoriesService } from './categories/categories.service';
 
-// Products
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 
-// Sales
 import { SalesController } from './sales/sales.controller';
 import { SalesService } from './sales/sales.service';
 
-// Payments
 import { PaymentsController } from './payments/payments.controller';
 import { PaymentsService } from './payments/payments.service';
 
