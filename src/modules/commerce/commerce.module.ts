2886c69cbbb3d7767225103dd4d5276f0e72e390
byna81 <fofana.naby81@gmail.com>
2026-03-18 15:10:45 +0100

    Update commerce.module.ts

--- src/modules/commerce/commerce.module.ts
+++ src/modules/commerce/commerce.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+
+// Categories
 import { CategoriesController } from './categories/categories.controller';
 import { CategoriesService } from './categories/categories.service';
+
+// Products
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 
+// Sales
+import { SalesController } from './sales/sales.controller';
+import { SalesService } from './sales/sales.service';
+
 @Module({
-  controllers: [CategoriesController, ProductsController],
-  providers: [CategoriesService, ProductsService],
+  controllers: [
+    CategoriesController,
+    ProductsController,
+    SalesController,
+  ],
+  providers: [
+    CategoriesService,
+    ProductsService,
+    SalesService,
+  ],
 })
 export class CommerceModule {}
