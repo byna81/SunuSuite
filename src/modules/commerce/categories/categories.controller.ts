a05e458e962d901104cd82cfa16ded2d9381978c
byna81 <fofana.naby81@gmail.com>
2026-03-19 16:05:29 +0100

    Update categories.controller.ts

--- src/modules/commerce/categories/categories.controller.ts
+++ src/modules/commerce/categories/categories.controller.ts
@@ -1,17 +1,27 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 
 @Controller('commerce/categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly service: CategoriesService) {}
 
   @Post()
   create(@Body() body: { tenantId: string; name: string }) {
-    return this.categoriesService.create(body);
+    return this.service.create(body);
   }
 
   @Get()
   findAll(@Query('tenantId') tenantId: string) {
-    return this.categoriesService.findAll(tenantId);
+    return this.service.findAll(tenantId);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: { name: string }) {
+    return this.service.update(id, body.name);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.service.delete(id);
   }
 }
