17b4c5db3cf7589e4bd0d984c8e8a3a562ee850d
byna81 <fofana.naby81@gmail.com>
2026-03-20 01:01:31 +0100

    Create tenants.service.ts

---
+++ src/tenants/tenants.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class TenantsService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(id: string) {
+    const tenant = await this.prisma.tenant.findUnique({
+      where: { id },
+    });
+
+    if (!tenant) {
+      throw new NotFoundException('Boutique introuvable');
+    }
+
+    return tenant;
+  }
+}
