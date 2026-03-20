20a196508011c0360098ecbaeafa070cbd5c8a53
byna81 <fofana.naby81@gmail.com>
2026-03-20 01:04:29 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 import { CommerceModule } from './modules/commerce/commerce.module';
+import { TenantsController } from './tenants/tenants.controller';
+import { TenantsService } from './tenants/tenants.service';
 
 @Module({
-  imports: [PrismaModule, CommerceModule],
-  controllers: [AppController],
+  imports: [CommerceModule],
+  controllers: [AppController, TenantsController],
+  providers: [PrismaService, TenantsService],
 })
 export class AppModule {}
