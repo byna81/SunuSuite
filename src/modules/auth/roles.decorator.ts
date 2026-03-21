4ffd9003a5537d6f19cf17d16e5460956a459a6f
byna81 <fofana.naby81@gmail.com>
2026-03-21 15:58:51 +0100

    Update roles.decorator.ts

--- src/modules/auth/roles.decorator.ts
+++ src/modules/auth/roles.decorator.ts
@@ -1,4 +1,5 @@
-import { SetMetadata } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {}
