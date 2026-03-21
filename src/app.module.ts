f44bd48a10d6a93978ae6178f697b24c6c5689b6
byna81 <fofana.naby81@gmail.com>
2026-03-21 12:50:04 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { CommerceModule } from './modules/commerce/commerce.module';
-import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { DashboardModule } from './modules/commerce/dashboard/dashboard.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TenantsController } from './tenants/tenants.controller';
 import { TenantsService } from './tenants/tenants.service';
