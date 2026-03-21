fe5ec8a2fb6d9c6a27e3fdf6da9243a1df150c91
byna81 <fofana.naby81@gmail.com>
2026-03-22 00:22:18 +0100

    Update auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,1 +1,21 @@
 
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'change_me_super_secret',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
