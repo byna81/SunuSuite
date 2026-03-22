547aaac73814e4df8367b42440741d1b1b31bbf1
byna81 <fofana.naby81@gmail.com>
2026-03-22 21:10:53 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CommerceModule } from './modules/commerce/commerce.module';
 import { DashboardModule } from './modules/commerce/dashboard/dashboard.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PropertyModule } from './modules/property/property.module';
 import { TenantsController } from './tenants/tenants.controller';
 import { TenantsService } from './tenants/tenants.service';
 
@@ -13,6 +14,7 @@ import { TenantsService } from './tenants/tenants.service';
     CommerceModule,
     DashboardModule,
     AuthModule,
+    PropertyModule,
   ],
   controllers: [
     AppController,
