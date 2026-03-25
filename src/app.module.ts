53f2e4344df7fbcd8f29110c9a46f0df2e6fd443
byna81 <fofana.naby81@gmail.com>
2026-03-25 23:53:23 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { CommerceModule } from './modules/commerce/commerce.module';
-import { DashboardModule } from './modules/commerce/dashboard/dashboard.module';
+import { DashboardModule as CommerceDashboardModule } from './modules/commerce/dashboard/dashboard.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PropertyModule } from './modules/property/property.module';
 import { TenantsController } from './tenants/tenants.controller';
@@ -10,19 +10,19 @@ import { TenantsService } from './tenants/tenants.service';
 import { ContractModule } from './modules/contract/contract.module';
 import { RentModule } from './modules/rent/rent.module';
 import { OwnerPaymentModule } from './modules/owner-payment/owner-payment.module';
-import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { DashboardModule as RealEstateDashboardModule } from './modules/dashboard/dashboard.module';
 
 @Module({
   imports: [
     PrismaModule,
     CommerceModule,
-    DashboardModule,
+    CommerceDashboardModule,
     AuthModule,
     PropertyModule,
     ContractModule,
     RentModule,
     OwnerPaymentModule,
-    DashboardModule,
+    RealEstateDashboardModule,
   ],
   controllers: [
     AppController,
