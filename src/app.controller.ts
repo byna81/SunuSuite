a19c42f69fc54ad03788e9804173d4f081842901
byna81 <fofana.naby81@gmail.com>
2026-03-30 20:33:51 +0000

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
-@Controller()
-export class AppController {
-  @Get()
-  health() {
-    return { ok: true, service: 'sunusuite-backend' };
-  }
-}
+import { AuthModule } from './modules/auth/auth.module';
+import { VehiclesModule } from './modules/vehicles/vehicles.module';
+import { VehicleCustomersModule } from './modules/vehicle-customers/vehicle-customers.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    VehiclesModule,
+    VehicleCustomersModule,
+  ],
+  providers: [PrismaService],
+})
+export class AppModule {}
