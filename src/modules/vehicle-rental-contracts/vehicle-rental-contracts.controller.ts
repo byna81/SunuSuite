778e21604ca0594d45e0c8cf953b8268e905abcc
byna81 <fofana.naby81@gmail.com>
2026-03-31 18:19:26 +0000

    Create vehicle-rental-contracts.controller.ts

---
+++ src/modules/vehicle-rental-contracts/vehicle-rental-contracts.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { VehicleRentalContractsService } from './vehicle-rental-contracts.service';
+
+@Controller('vehicle-rental-contracts')
+@UseGuards(JwtAuthGuard)
+export class VehicleRentalContractsController {
+  constructor(private readonly service: VehicleRentalContractsService) {}
+
+  @Get()
+  findAll(@Req() req: any) {
+    return this.service.findAll(req.user.tenantId);
+  }
+
+  @Get(':id')
+  findOne(@Req() req: any, @Param('id') id: string) {
+    return this.service.findOne(req.user.tenantId, id);
+  }
+
+  @Post()
+  create(@Req() req: any, @Body() body: any) {
+    return this.service.create(req.user.tenantId, body);
+  }
+
+  @Post(':id/payments')
+  addPayment(@Req() req: any, @Param('id') id: string, @Body() body: any) {
+    return this.service.addPayment(req.user.tenantId, id, body);
+  }
+}
