f470231a4752ee66de394a5cc51a7f961e7a01f3
byna81 <fofana.naby81@gmail.com>
2026-04-02 00:14:18 +0000

    Update admin.module.ts

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
-import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { AdminController } from './admin.controller';
+import { PrismaService } from '../../prisma/prisma.service';
+
+import { MailModule } from '../mail/mail.module';
+import { ContractsModule } from '../contracts/contracts.module';
 
 @Module({
+  imports: [
+    MailModule,
+    ContractsModule,
+  ],
   controllers: [AdminController],
   providers: [AdminService, PrismaService],
 })
