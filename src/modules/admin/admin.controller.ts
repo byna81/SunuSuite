988d24b1f8ffd53ac7a7fb598fa3d7d0bd224636
byna81 <fofana.naby81@gmail.com>
2026-04-02 13:06:26 +0000

    Update admin.controller.ts

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -20,6 +20,21 @@ export class AdminController {
     return this.adminService.validateRequest(id);
   }
 
+  @Patch('requests/:id/reject')
+reject(@Param('id') id: string) {
+  return this.adminService.rejectRequest(id);
+}
+
+@Patch('tenants/:id/suspend')
+suspend(@Param('id') id: string) {
+  return this.adminService.suspendTenant(id);
+}
+
+@Patch('tenants/:id/reactivate')
+reactivate(@Param('id') id: string) {
+  return this.adminService.reactivateTenant(id);
+}
+
   @Get('tenants')
   getTenants() {
     return this.adminService.getTenants();
