aba749a367e5888b412fcac8e88d98198a5c0b6b
byna81 <fofana.naby81@gmail.com>
2026-04-03 17:19:27 +0000

    Update mail.module.ts

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
-import { MailService } from './mail.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  providers: [MailService],
-  exports: [MailService],
+  imports: [
+    PrismaModule,
+    MailModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'super-secret-key',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
-export class MailModule {}
+export class AuthModule {}
