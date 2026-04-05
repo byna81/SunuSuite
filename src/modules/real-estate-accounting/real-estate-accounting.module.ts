f4b0e18de23d1c7c809352faf908502cb5128b5c
byna81 <fofana.naby81@gmail.com>
2026-04-05 18:31:37 +0000

    Create real-estate-accounting.module.ts

---
+++ src/modules/real-estate-accounting/real-estate-accounting.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { RealEstateAccountingController } from './real-estate-accounting.controller';
+import { RealEstateAccountingService } from './real-estate-accounting.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [RealEstateAccountingController],
+  providers: [RealEstateAccountingService],
+  exports: [RealEstateAccountingService],
+})
+export class RealEstateAccountingModule {}
