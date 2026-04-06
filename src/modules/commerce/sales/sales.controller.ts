e0696b8898c993a8e7ec92e1727cb176ef80346d
byna81 <fofana.naby81@gmail.com>
2026-04-06 03:11:29 +0000

    Update sales.controller.ts

--- src/modules/commerce/sales/sales.controller.ts
+++ src/modules/commerce/sales/sales.controller.ts
@@ -25,7 +25,7 @@ export class SalesController {
     },
     @Req() req: any,
   ) {
-    const cashierId = req?.user?.id || null;
+    const cashierId = req?.user?.id || req?.user?.sub || null;
 
     return this.salesService.create({
       tenantId: body.tenantId,
