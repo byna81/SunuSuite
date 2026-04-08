884e585ed2b337839a8fadcaef4bcf5ac6ee7724
byna81 <fofana.naby81@gmail.com>
2026-04-08 00:14:26 +0000

    Update vehicle-payment.module.ts

--- src/modules/vehicle-payment/vehicle-payment.module.ts
+++ src/modules/vehicle-payment/vehicle-payment.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { VehiclePaymentService } from './vehicle-payment.service';
 import { VehiclePaymentController } from './vehicle-payment.controller';
+import { VehiclePaymentService } from './vehicle-payment.service';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [VehiclePaymentController],
   providers: [VehiclePaymentService, PrismaService],
+  exports: [VehiclePaymentService],
 })
 export class VehiclePaymentModule {}
