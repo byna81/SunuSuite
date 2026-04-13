5793eb7efb80d498692d19f63c820d117975b4fe
byna81 <fofana.naby81@gmail.com>
2026-04-13 22:11:53 +0200

    Update owners.module.ts

--- src/modules/Owners/owners.module.ts
+++ src/modules/Owners/owners.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { OwnersService } from './owners.service';
 import { OwnersController } from './owners.controller';
+import { OwnersService } from './owners.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [OwnersController],
-  providers: [OwnersService],
+  providers: [OwnersService, PrismaService],
+  exports: [OwnersService],
 })
 export class OwnersModule {}
