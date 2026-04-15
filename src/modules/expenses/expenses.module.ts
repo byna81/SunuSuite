950928810d1f41a1d6f1658c098bc2ed308137ec
byna81 <fofana.naby81@gmail.com>
2026-04-15 17:27:06 +0200

    Update expenses.module.ts

--- src/modules/expenses/expenses.module.ts
+++ src/modules/expenses/expenses.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
-import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
