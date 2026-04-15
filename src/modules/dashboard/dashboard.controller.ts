df4ed15bb8b3c3c6ee411f1e33d658534115c066
byna81 <fofana.naby81@gmail.com>
2026-04-15 16:13:03 +0200

    Update dashboard.controller.ts

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -13,6 +13,18 @@ export class DashboardController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @Get('sale')
+  getSaleDashboard(@Req() req: any) {
+    return this.service.getSaleDashboard(req.user.tenantId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('rental')
+  getRentalDashboard(@Req() req: any) {
+    return this.service.getRentalDashboard(req.user.tenantId);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get('yango')
   getYangoDashboard(@Req() req: any) {
     return this.service.getYangoDashboard(req.user.tenantId);
