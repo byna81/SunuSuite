51f39ae01a0c12f6b9a0dfbb6581cb40b99e4887
byna81 <fofana.naby81@gmail.com>
2026-04-16 23:03:00 +0200

    Update agent.controller.ts

--- src/modules/agent/agent.controller.ts
+++ src/modules/agent/agent.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   Patch,
@@ -45,9 +46,9 @@ export class AgentController {
   resetPassword(@Req() req: any, @Param('id') id: string, @Body() body: any) {
     return this.agentService.resetPassword(req.user, id, body);
   }
-  @Delete('agents/:id')
-@UseGuards(JwtAuthGuard)
-deleteAgent(@Param('id') id: string, @Req() req: any) {
-  return this.service.deleteAgent(id, req.user.tenantId);
-}
+
+  @Delete(':id')
+  delete(@Req() req: any, @Param('id') id: string) {
+    return this.agentService.delete(req.user, id);
+  }
 }
