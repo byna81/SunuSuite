9ce61b27ee9bbd3c1f853251e27cffce02aafe27
byna81 <fofana.naby81@gmail.com>
2026-04-17 23:47:42 +0200

    Update admin-system.module.ts

--- src/modules/admin-system/admin-system.module.ts
+++ src/modules/admin-system/admin-system.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AdminSystemService } from './admin-system.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AdminSystemService } from './admin-system.service';
+import { AdminSystemController } from './admin-system.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [AdminSystemService],
+  controllers: [AdminSystemController],
 })
 export class AdminSystemModule {}
