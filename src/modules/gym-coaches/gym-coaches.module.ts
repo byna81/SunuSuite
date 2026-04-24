a1bbef564192dc17ac594cc2a3038d5d7a12d1de
byna81 <fofana.naby81@gmail.com>
2026-04-24 17:38:03 +0200

    Update gym-coaches.module.ts

--- src/modules/gym-coaches/gym-coaches.module.ts
+++ src/modules/gym-coaches/gym-coaches.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GymCoachesController } from './gym-coaches.controller';
 import { GymCoachesService } from './gym-coaches.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [GymCoachesController],
