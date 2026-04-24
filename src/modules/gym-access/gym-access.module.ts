5f40a71177f0c0fc5cf16af21ebcf8549fe7090a
byna81 <fofana.naby81@gmail.com>
2026-04-24 18:24:25 +0200

    Update gym-access.module.ts

--- src/modules/gym-access/gym-access.module.ts
+++ src/modules/gym-access/gym-access.module.ts
@@ -1,1 +1,10 @@
+import { Module } from '@nestjs/common';
+import { GymAccessController } from './gym-access.controller';
+import { GymAccessService } from './gym-access.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
+@Module({
+  controllers: [GymAccessController],
+  providers: [GymAccessService, PrismaService],
+})
+export class GymAccessModule {}
