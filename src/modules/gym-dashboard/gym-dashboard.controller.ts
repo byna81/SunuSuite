3d50118facde69e7f841e254ff2a8c15bbb55ec7
byna81 <fofana.naby81@gmail.com>
2026-04-28 23:22:17 +0200

    Create gym-dashboard.controller.ts

---
+++ src/modules/gym-dashboard/gym-dashboard.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { GymDashboardService } from './gym-dashboard.service';
+
+@Controller('gym/dashboard')
+export class GymDashboardController {
+  constructor(private readonly service: GymDashboardService) {}
+
+  @Get()
+  getDashboard(@Query('tenantId') tenantId: string) {
+    return this.service.getDashboard(tenantId);
+  }
+}
