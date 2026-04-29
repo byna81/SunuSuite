06dd8577904c88aac0ee48956aa9d007932f4d9a
byna81 <fofana.naby81@gmail.com>
2026-04-29 10:54:52 +0200

    Update gym-dashboard.module.ts

--- src/modules/gym-dashboard/gym-dashboard.module.ts
+++ src/modules/gym-dashboard/gym-dashboard.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GymDashboardController } from './gym-dashboard.controller';
 import { GymDashboardService } from './gym-dashboard.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [GymDashboardController],
