3e9952eda9a9bf6797588dcd647ae1592c4167ee
byna81 <fofana.naby81@gmail.com>
2026-05-01 01:33:18 +0200

    Update gym-access.controller.ts

--- src/modules/gym-access/gym-access.controller.ts
+++ src/modules/gym-access/gym-access.controller.ts
@@ -4,7 +4,13 @@ import { GymAccessService } from './gym-access.service';
 @Controller('gym-access')
 export class GymAccessController {
   constructor(private readonly service: GymAccessService) {}
-
+@Post('use')
+usePass(
+  @Query('tenantId') tenantId: string,
+  @Body() body: any,
+) {
+  return this.service.usePass(tenantId, body);
+}
   @Get('check')
   async check(@Query('qrCode') qrCode: string) {
     if (!qrCode) throw new BadRequestException('qrCode obligatoire');
