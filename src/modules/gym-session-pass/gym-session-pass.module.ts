f0841e5c67473171412056ae5e9573eae4c3e280
byna81 <fofana.naby81@gmail.com>
2026-04-30 11:02:15 +0200

    Update gym-session-pass.module.ts

--- src/modules/gym-session-pass/gym-session-pass.module.ts
+++ src/modules/gym-session-pass/gym-session-pass.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { GymSessionPassController } from "./gym-session-pass.controller";
 import { GymSessionPassService } from "./gym-session-pass.service";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../../prisma/prisma.service";
 
 @Module({
   controllers: [GymSessionPassController],
